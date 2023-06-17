14ecb0541195f052c2e185cfe945aed2e7d36438
angelMarcosCastilla <ajmarcosc15@gmail.com>
2023-06-17 08:19:29 -0500

    add eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard-with-typescript', 'plugin:astro/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        'no-tabs': 'off'
+      }
+    }
+  ]
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	printWidth: 100,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'none',
+	useTabs: false,
+	endOfLine: 'lf',
+	bracketSpacing: true,
+	quoteProps: 'as-needed',
+}
