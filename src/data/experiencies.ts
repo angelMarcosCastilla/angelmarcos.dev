364becfa46c3859f6d9a8aef5712c9c9b3ddf688
angelMarcosCastilla <ajmarcosc15@gmail.com>
2024-01-17 12:21:34 -0500

    feat: add experencie laboral

---
+++ src/data/experiencies.ts
@@ -0,0 +1,15 @@
+interface IExperiencia {
+  time: string
+  role: string
+  company: string
+  description: string
+}
+
+export const EXPERIENCIES: IExperiencia[] = [
+  { 
+  time: 'Marzo 2022 - Actualmemnte', 
+  role: 'Junior Frontend developer', 
+  company: 'Latamready', 
+  description: 'Formo parte del equipo que desarrolla herramientas internas para la empresa como un administrador de proyecto para dar segumiento a las tareas de los desarollo usando React para la parte de frontend'
+  }
+]
