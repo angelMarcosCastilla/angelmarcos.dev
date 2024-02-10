847cb30fb24d13e8f75ce1dd6726eb3fecc1623c
angelMarcosCastilla <ajmarcosc15@gmail.com>
2024-02-10 10:20:18 -0500

    fix: spelling errors in expreciencies

--- src/data/experiencies.ts
+++ src/data/experiencies.ts
@@ -10,6 +10,6 @@ export const EXPERIENCIES: IExperiencia[] = [
   time: 'Marzo 2022 - Actualmemnte', 
   role: 'Junior Frontend developer', 
   company: 'Latamready', 
-  description: 'Formo parte del equipo que desarrolla herramientas internas para la empresa como un administrador de proyecto para dar segumiento a las tareas de los desarollo usando React para la parte de frontend'
+  description: 'Formo parte del equipo que desarrolla herramientas internas para la empresa como un administrador de proyecto para dar seguimiento a las tareas de los desarrollos, usando React para la parte de frontend.'
   }
 ]
