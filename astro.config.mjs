6dbbe41e89d666180dfc151bfc2cc9d8f519c214
angelMarcosCastilla <ajmarcosc15@gmail.com>
2026-04-29 07:39:17 -0500

    Refactor SVG icons for consistency and readability; update attributes and remove unnecessary classes. Add content configuration for blog posts and introduce skills data structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from '@astrojs/svelte'
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: 'hu',
+      theme: 'github-light',
     },
   },
   integrations: [
---
+++ src/data/skills.ts
@@ -0,0 +1,16 @@
+export const SKILLS = [
+  { id: 'html', name: 'HTML' },
+  { id: 'css', name: 'CSS' },
+  { id: 'js', name: 'JavaScript' },
+  { id: 'react', name: 'React' },
+  { id: 'materialui', name: 'Material UI' },
+  { id: 'reactroute', name: 'React Router' },
+  { id: 'next', name: 'Next.js' },
+  { id: 'astro', name: 'Astro' },
+  { id: 'svelte', name: 'Svelte' },
+  { id: 'node', name: 'Node.js' },
+  { id: 'mysql', name: 'MySQL' },
+  { id: 'tailwind', name: 'Tailwind' },
+  { id: 'redux', name: 'Redux' },
+  { id: 'php', name: 'PHP' }
+]
